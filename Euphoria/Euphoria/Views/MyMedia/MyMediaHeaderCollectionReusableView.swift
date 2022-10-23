c331e6822c65552c93de013cd0ac3699bedc3a82
Anna Ogorodova <renita5701@gmail.com>
2022-10-23 23:47:30 +0600

    Added albums

---
+++ Euphoria/Euphoria/Views/MyMedia/MyMediaHeaderCollectionReusableView.swift
@@ -0,0 +1,36 @@
+//
+//  MyMediaHeaderCollectionReusableView.swift
+//  Euphoria
+//
+//  Created by Anna Ogorodova on 23.10.2022.
+//
+
+
+import UIKit
+
+protocol MyMediaHeaderCollectionReusableViewDelegate {
+    func addNewPlaylist()
+    func addNewAlbum()
+}
+
+class MyMediaHeaderCollectionReusableView: UICollectionReusableView {
+    static let identifier = "MyMediaHeaderCollectionReusableView"
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    static var delegate: MyMediaHeaderCollectionReusableViewDelegate?
+    
+    func configure(with title: String) {
+        titleLabel.text = title
+    }
+    
+    @IBAction func addNewPlaylist(_ sender: UIButton) {
+        if titleLabel.text == "Playlists" {
+            MyMediaHeaderCollectionReusableView.delegate?.addNewPlaylist()
+        } else if titleLabel.text == "Albums"{
+            MyMediaHeaderCollectionReusableView.delegate?.addNewAlbum()
+        }
+    }
+}
+
